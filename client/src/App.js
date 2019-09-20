27e4a2033a08ff1d89afd7419ca1d1a89202ea71
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-20 08:19:49 -0700

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,16 @@ import BubblesPage from "./components/BubblePage";
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
 import "./styles.scss";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [colorList, setColorList] = useState([]);
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <Route exact path="/" component={Login} />
-        <PrivateRoute exact path="/BubblesPage" component={BubblesPage} />
+        <PrivateRoute exact path="/bubblespage" component={BubblesPage} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
