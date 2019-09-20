fe0b72708b5812e614522938ee37eb88df83aa6b
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-20 08:34:54 -0700

    building login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [login, setLogin] = useState({
+    username: "",
+    password: ""
+  });
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <form>
+        <field>
+          UserName
+          <input type="text" name={"username"} placeholder="username" />
+        </field>
+        <field>
+          password
+          <input type="password" name={"password"} placeholder="password" />
+        </field>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 };
 
